0f162c7b437c6be74a71884c1adb32dfcd4debf9
gloriamaris <monique.dingding@gmail.com>
2018-11-30 00:22:35 +0800

    delete venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/monique/Documents/Projects/Personal/designsouthreg/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
